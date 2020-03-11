7f19c78c28ac34e8f0b05c09907ba0ae53a67562
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-11 03:03:06 +0000

    Deploy monzter50/poke-infinitescroll to github.com/monzter50/poke-infinitescroll.git:gh-pages

---
+++ precache-manifest.af668e7d2ff44c37b31d0e8f401f1f84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030f8cb342d1e13eec09efef8ac38933",
+    "url": "/poke-infinitescroll/index.html"
+  },
+  {
+    "revision": "cc63bfd341ede3d1575e",
+    "url": "/poke-infinitescroll/static/css/main.68093335.chunk.css"
+  },
+  {
+    "revision": "76953e82f1a434e3ce2e",
+    "url": "/poke-infinitescroll/static/js/2.aa93e0a0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/poke-infinitescroll/static/js/2.aa93e0a0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc63bfd341ede3d1575e",
+    "url": "/poke-infinitescroll/static/js/main.f1345a06.chunk.js"
+  },
+  {
+    "revision": "6be08804589bbfee66c6",
+    "url": "/poke-infinitescroll/static/js/runtime-main.b4c28b67.js"
+  },
+  {
+    "revision": "86a7ae01453508a4decd73573eed983d",
+    "url": "/poke-infinitescroll/static/media/pokeball.86a7ae01.svg"
+  }
+]);
